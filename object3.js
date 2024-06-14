82e56db9fb19c8a093bb8a077b2aebd84666a2b6
ParkkKU <pku02089@gmail.com>
2024-06-14 14:53:30 +0900

    240614 3시

---
+++ object3.js
@@ -0,0 +1,31 @@
+function modifyUser(user, target, value) {
+    const newUser = {
+        ...user,
+        [target]: value
+    };
+    return newUser;
+}
+
+function main() {
+    const user = {
+        username : "admin",
+        password : "1234"
+    }
+
+    console.log(user);
+
+    const newUser = modifyUser(user, "username", "test-user");
+    console.log(newUser);
+
+    const newUser2 = modifyUser(user, "password", "1111");
+    console.log(newUser2);
+
+    const userlist = [ user, newUser ];     //100
+    const newUserList = [ ...userList, newUser2 ];    //200
+
+    // 스프레드 -> 깊은 복사 (안에 있는 값 새로운 주소에)
+    
+    const userList2 = userList; // 얕은 복사
+}
+
+main();
---
+++ object4.js
@@ -0,0 +1,12 @@
+//비구조 할당
+
+function main() {
+    const user = {
+        username: "admin",
+        password: "1234",
+        name: "김준일",
+        email: "aaa@gmail.com"
+    }
+
+    const names = [ "박현주", "이성희", "권오광", "권혁진" ];
+}
